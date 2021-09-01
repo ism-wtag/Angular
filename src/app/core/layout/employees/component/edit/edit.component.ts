07eb7e0b50804e1add45a9ba5867dc4bd9c42abc
ism-wtag <mujahidul.islam@welldev.io>
2021-09-01 14:30:30 +0600

    LMS-888-Angular-form-concept

--- src/app/core/layout/employees/component/edit/edit.component.ts
+++ src/app/core/layout/employees/component/edit/edit.component.ts
@@ -1,47 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Employee } from 'src/app/models/employee.model';
-import { EmployeeService } from 'src/app/services/employee.service';
+import { Employee } from 'src/app/core/layout/employees/models/employee.model';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-  id: number ;
+  id: number;
   header: string;
   employee: Employee = {
     id: 0,
-    name: "",
-    email: "",
-    phone: ""
-  }
+    name: '',
+    email: '',
+    phone: '',
+  };
 
-  constructor(private router: Router, private route: ActivatedRoute, private employeeService: EmployeeService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private employeeService: EmployeeService
+  ) {}
 
   ngOnInit(): void {
     this.id = +this.route.snapshot.paramMap.get('id');
     this.header = this.id === 0 ? 'Add Employee' : 'Edit Employee';
 
-    if(this.id != 0 ){
+    if (this.id != 0) {
       this.employee = this.employeeService.onGetEmployee(this.id);
     }
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {
     let employee: Employee = {
       id: form.value.id,
       name: form.value.name,
       email: form.value.email,
       phone: form.value.phone,
-    }
+    };
 
-    if(this.id === 0){
+    if (this.id === 0) {
       this.employeeService.onAdd(employee);
-    }
-    else{
+    } else {
       this.employeeService.onUpdate(employee);
     }
     this.router.navigateByUrl('');
